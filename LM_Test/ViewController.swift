78b2e16ba2bd66ce11d9be583347ad720b8f56e8
Nikita Gvozdikov <pounddnuop@mail.ru>
2021-04-18 12:50:11 +0300

    Initial Commit

---
+++ LM_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LM_Test
+//
+//  Created by Никита Гвоздиков on 18.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
